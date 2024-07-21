18eca556dc924776d053bed53930c9d9ce759012
YGS <tig03027@naver.com>
2024-07-21 13:50:35 +0900

    FEAT basic component, Autoplay plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import MainSlide from "@/components/main_slide/MainSlide";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <section className="main_slide_section"></section>
+      <MainSlide />
+      <section className="move_slide_section">
+        <article className="movie_slide">
+          <h2></h2>
 
-    </div>
+          <div className="movie">
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <h3></h3>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
