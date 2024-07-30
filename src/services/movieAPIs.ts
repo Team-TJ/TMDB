97fc639c374896922974deb12d8714a78b826377
happysuwonboy <yhoonsong96@gmail.com>
2024-07-30 18:00:00 +0900

    [Feat] 영화 데이터 패치 함수 추가

---
+++ src/services/movieAPIs.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiKey = process.env.TMDB_API_KEY;
+
+
+export const fetchMovieDetail = async (movieId:string, lang : string = 'en-US') => {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=${lang}`); 
+    return response.data;
+}
+
+export const fetchMovieCredits = async (movieId : string, lang : string = 'en-US') => {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=${lang}`);
+    return response.data;
+}
+
