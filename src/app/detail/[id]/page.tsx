a5bdd3cf841de30f7b904b81098fae9faba56de5
happysuwonboy <yhoonsong96@gmail.com>
2024-07-30 18:01:32 +0900

    [Refactor] 영화 데이터 패칭 부분 리팩토링

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import MovieBackdrop from "@/container/detail/MovieBackdrop";
 import MovieDetailSection from "@/container/detail/MovieDetailSection";
-import Image from "next/image";
-import { BiMovie } from "react-icons/bi";
+import { fetchMovieDetail } from "@/services/movieAPIs";
 
 interface MovieDetailPageProps {
   params: {
@@ -11,12 +10,10 @@ interface MovieDetailPageProps {
 }
 
 const Page: React.FC<MovieDetailPageProps> = async ({ params }) => {
-  let apiKey = "7105e5b5b38ae5649d69f0e2639eb114";
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${params.id}?api_key=${apiKey}`
-  );
-  const { title, poster_path, backdrop_path, overview, original_title } =
-    response.data;
+  const movieData = await fetchMovieDetail(params.id)
+  
+  
+  const { title, poster_path, backdrop_path, overview, original_title, production_companies } = movieData;
 
   return (
     <div>
@@ -30,6 +27,7 @@ const Page: React.FC<MovieDetailPageProps> = async ({ params }) => {
 
       {/* 영화 상세정보 섹션 */}
       <MovieDetailSection 
+        id={params.id}
         title={title} 
         poster_path={poster_path}
         overview={overview}
