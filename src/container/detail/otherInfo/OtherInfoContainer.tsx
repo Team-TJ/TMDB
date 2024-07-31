caa35105cbcd717290e715a32219b093a743b1c5
happysuwonboy <yhoonsong96@gmail.com>
2024-07-31 17:57:33 +0900

    [Fix] 영화 크레딧 정보 UI 마지막 요소 아래 border 안보이도록 수정

--- src/container/detail/otherInfo/OtherInfoContainer.tsx
+++ src/container/detail/otherInfo/OtherInfoContainer.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const OtherInfoContainer = ({children} : {children : ReactNode}) => {
   return (
-    <ul className="bg-[rgba(255,255,255,0.1)] py-3 px-5 mt-5 italic">
+    <ul className="bg-[rgba(255,255,255,0.1)] py-3 px-5 mt-5 italic last:border-none">
         {children}
     </ul>
   );
--- src/container/detail/otherInfo/OtherInfoItem.tsx
+++ src/container/detail/otherInfo/OtherInfoItem.tsx
@@ -9,13 +9,13 @@ interface OtherInfoItemProps {
 
 const OtherInfoItem : React.FC<OtherInfoItemProps> = ({title, titleIcon, content}) => {
   return (
-    <li className="py-2 flex items-center border-b-[#ffffff34] border-b-[.3px] gap-10">
+    <li className="py-2 flex items-center gap-10 border-b-[#ffffff34] border-b-[.3px] last:border-b-0">
         <div className="flex items-center gap-2 w-[60px]">
            {titleIcon}
           <span className="text-[#c5cfe0d2] text-[12.6px]">{title}</span>
         </div>
-        <div className={cn("text-[#f9f9fa] text-[14px]", content || 'text-[#b9b9bbe1]')}>
-            {content || `${title} 정보 없음`}
+        <div className={cn("text-[#f9f9fa] text-[14px] truncate", content || 'text-[#b9b9bbe1]')}>
+            <span>{content || `${title} 정보 없음`}</span>
         </div>
     </li>
   )
