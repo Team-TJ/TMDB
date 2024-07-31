1ad007e2c72fa7e623f3d0a9142658bb126f823c
happysuwonboy <yhoonsong96@gmail.com>
2024-07-31 18:01:46 +0900

    [Feat] 상세페이지 backdrop 영역 작업중

--- src/container/detail/MovieBackdrop.tsx
+++ src/container/detail/MovieBackdrop.tsx
@@ -1,12 +1,15 @@
+import { fetchMovieImages } from "@/services/movieAPIs";
 import Image from "next/image";
 
-const Page = ({ imgPath }: { imgPath: string }) => {
+const MovieBackdrop = async ({ id }: { id: string }) => {
+  const movieImages = await fetchMovieImages(id);
+  
   return (
     <div className="absolute top-[80px] w-full h-[300px] overflow-hidden  z-[-1]">
       <Image
         fill
         className="scale-[1] object-cover object-center lg:brightness-[50%]"
-        src={`https://image.tmdb.org/t/p/original/${imgPath}`}
+        src={`https://image.tmdb.org/t/p/original/${movieImages.backdrops[0].file_path}`}
         alt="이미지"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#091A38] from-[5%]"></div>
@@ -14,4 +17,4 @@ const Page = ({ imgPath }: { imgPath: string }) => {
   );
 };
 
-export default Page;
+export default MovieBackdrop;
