608a025ef24dde3ea90094fb23aea03441290b36
YGS <tig03027@naver.com>
2024-08-06 19:53:04 +0900

    FEAT : MainSlide, MovieSlide, pngs...

---
+++ src/components/movie_slide/MovieSlide.tsx
@@ -0,0 +1,63 @@
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { SiThemoviedatabase } from "react-icons/si";
+import { BsCalendarDayFill } from "react-icons/bs";
+import { MdInsertChart } from "react-icons/md";
+import { FaComments } from "react-icons/fa6";
+import MovieContent from "../movie/MovieContent";
+interface MovieSlideType {
+  title?: string;
+}
+export default function MovieSlide(props: MovieSlideType) {
+  return (
+    <>
+      <article className="py-[40px] w-[min(90%,1600px)] my-0 mx-auto">
+        <h2 className="flex items-center mb-5 text-2xl after:flex-1 after:content-[''] after:ml-[10px] after:border-t-[1px] after:border-solid after:border-white/30">
+          {props.title === "popular" && (
+            <>
+              <SiThemoviedatabase className="text-[#03b3e4] mr-[10px]" />
+              <em className="font-semibold text-white">현재 인기 영화</em>
+            </>
+          )}
+          {props.title === "new" && (
+            <>
+              <BsCalendarDayFill className="text-[#03b3e4] mr-[10px]" />
+              <em className="font-semibold text-white">최신, 개봉 예정 영화</em>
+            </>
+          )}
+          {props.title === "rating" && (
+            <>
+              <MdInsertChart className="text-[#03b3e4] mr-[10px]" />
+              <em className="font-semibold text-white">평점이 높은 영화</em>
+            </>
+          )}
+          {props.title === "weekend" && (
+            <>
+              <FaComments className="text-[#03b3e4] mr-[10px]" />
+              <em className="font-semibold text-white">주간 화제 영화</em>
+            </>
+          )}
+        </h2>
+        <div>
+          <Carousel
+            className="w-full"
+            opts={{ loop: true, align: "center" }}
+            plugins={[]}
+          >
+            <CarouselContent className="">
+              {Array.from({ length: 10 }).map((_, index) => (
+                <CarouselItem className="basis-auto" key={index}>
+                  <MovieContent index={index} />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      </article>
+    </>
+  );
+}
