2405817cdc4970f2a3a0c029785a21a71ac99509
YGS <tig03027@naver.com>
2024-09-24 20:59:07 +0900

    FIX : Spinner.tsx 메인에서만 작동

--- src/components/loading_spinner/Spinner.tsx
+++ src/components/loading_spinner/Spinner.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useLoadingStore } from "@/states/loading";
+import { usePathname } from "next/navigation";
 
 export default function Spinner() {
+  const pathName = usePathname();
+  console.log(pathName);
   const isLoadingCnt = useLoadingStore((state) => state.isLoadingCnt);
   const isLoadingYet = useLoadingStore((state) => state.isLoadingYet);
-  if (isLoadingCnt !== 0 || isLoadingYet) {
+  if (isLoadingCnt !== 0 || (isLoadingYet && pathName === "/")) {
     return (
       <div
         className={
