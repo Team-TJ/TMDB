eb9c9e7dc8fe58506a60430bd6dbb1f39ce4f25f
syh <yhoonsong96@gmail.com>
2024-10-20 19:39:26 +0900

    [Feat] 영화 이미지 섹션 1차 구현 완료

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,9 +1,13 @@
-
-import axios from "axios";
-import MovieBackdrop from "@/container/detail/MovieBackdrop";
-import MovieDetailSection from "@/container/detail/MovieDetailSection";
-import ActorSection from "@/container/detail/actor/ActorSection";
-import { fetchMovieImages, fetchMovieCredits, fetchMovieDetail } from "@/services/movieAPIs";
+import axios from 'axios';
+import MovieBackdrop from '@/container/detail/MovieBackdrop';
+import MovieDetailSection from '@/container/detail/MovieDetailSection';
+import ActorSection from '@/container/detail/actor/ActorSection';
+import {
+  fetchMovieImages,
+  fetchMovieCredits,
+  fetchMovieDetail,
+} from '@/services/movieAPIs';
+import { MovieImageSection } from '@/container/detail/MovieImageSection';
 
 interface MovieDetailPageProps {
   params: {
@@ -15,8 +19,8 @@ const Page: React.FC<MovieDetailPageProps> = async ({ params }) => {
   const [movieDetail, movieCredits, movieImages] = await Promise.all([
     fetchMovieDetail(params.id),
     fetchMovieCredits(params.id),
-    fetchMovieImages(params.id)
-  ]) 
+    fetchMovieImages(params.id),
+  ]);
 
   return (
     <div className="pt-16 lg:pt-20 min-h-[100vh]">
@@ -24,11 +28,20 @@ const Page: React.FC<MovieDetailPageProps> = async ({ params }) => {
       <MovieBackdrop images={movieImages.backdrops} />
 
       {/* 영화 상세정보 섹션 */}
-      <MovieDetailSection movieCredits={movieCredits} movieDetail={movieDetail} />
+      <MovieDetailSection
+        movieCredits={movieCredits}
+        movieDetail={movieDetail}
+      />
 
       {/* 출연진 섹션 */}
-      <ActorSection movieCredits={movieCredits}/> 
-      
+      <ActorSection movieCredits={movieCredits} />
+
+      <div className='mb-[50px]'></div>
+
+      <MovieImageSection images={movieImages.backdrops} />
+
+      <div className='mb-[50px]'></div>
+
       
     </div>
   );
---
+++ src/container/detail/MovieImageSection.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React, { useState } from 'react';
+import SectionTitle from '@/components/SectionTitle';
+import { FaRegImages } from 'react-icons/fa';
+import Image from 'next/image';
+import getImagePath from '@/utils/getImagePath';
+import FsLightbox from 'fslightbox-react';
+
+interface MovieImageSectionProps {
+  images: any;
+}
+
+export const MovieImageSection: React.FC<MovieImageSectionProps> = ({
+  images,
+}) => {
+  const [lightboxController, setLightboxController] = useState({
+    toggler: false,
+    slide: 1,
+  });
+
+  function openLightboxOnSlide(number: number) {
+    setLightboxController({
+      toggler: !lightboxController.toggler,
+      slide: number,
+    });
+  }
+
+  return (
+    <section className="px-[5%] max-w-[1600px] mx-auto">
+      <SectionTitle
+        className="mt-8 mb-0"
+        icon={<FaRegImages color={'#03b3e4'} size={28} />}
+        text="관련 이미지"
+      />
+      <ul className="flex overflow-x-auto gap-[15px] py-4">
+        {images.slice(0, 20).map((image: any, idx: number) => {
+          return (
+            <li
+              key={idx}
+              className="flex-shrink-0 group cursor-pointer border-[#EEEEEE] border-solid border-[5px]"
+              onClick={() => openLightboxOnSlide(idx + 1)}
+            >
+              <div className="w-[240px] h-[135px] relative overflow-hidden">
+                <Image
+                  fill
+                  src={getImagePath(image.file_path) || '/film1.jpg'}
+                  className="object-cover group-hover:scale-110 transition-all duration-500"
+                  alt="영화 사진"
+                />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <FsLightbox
+        toggler={lightboxController.toggler}
+        sources={images
+          .slice(0, 20)
+          .map((image: any) => getImagePath(image.file_path))}
+        slide={lightboxController.slide}
+      />
+    </section>
+  );
+};
