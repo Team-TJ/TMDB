f146335212b724e35d44aefc4cffd93610389da1
YHSONG\yhoon <yhoonsong96@gmail.com>
2024-10-25 01:43:58 +0900

    [Fix] 출연진 섹션, 이미지 섹션 정보 없는 경우 예외처리

--- src/container/detail/actor/ActorSection.tsx
+++ src/container/detail/actor/ActorSection.tsx
@@ -27,13 +27,19 @@ const ActorSection = ({ movieCredits }: { movieCredits: any }) => {
     <>
       <section className='px-[5%] max-w-[1600px] mx-auto'>
         <SectionTitle className='mt-8 mb-0' icon={<IoMdPeople color={'#03b3e4'} size={28} />} text='출연진' />
-        <ul className='flex overflow-x-auto gap-[15px] py-4'>
-          {mainCast.map((actor: any) => {
-            return (
-              <ActorCard key={actor.id} actor={actor} onClick={handleModalShow(actor)} />
-            );
-          })}
-        </ul>
+        {mainCast.length ?
+          <ul className='flex overflow-x-auto gap-[15px] py-4'>
+            {mainCast.map((actor: any) => {
+              return (
+                <ActorCard key={actor.id} actor={actor} onClick={handleModalShow(actor)} />
+              );
+            })}
+          </ul>
+          :
+          <div className='flex justify-center items-center text-[#CCCCCC] h-[100px]'>
+            출연진 정보가 없습니다.
+          </div>
+        }
       </section>
       {modalConfig.isShow ? <ActorModal actorId={modalConfig.actorId} closeModal={handleModalClose} /> : null}
     </>
