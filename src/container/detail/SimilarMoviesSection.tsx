27cdb6d6102bb7e31b0dc49415e65f6a3d378e5c
YHSONG\yhoon <yhoonsong96@gmail.com>
2024-10-26 16:09:31 +0900

    [Feat] 유사한 영화 섹션 반응형 구현

--- src/container/detail/SimilarMoviesSection.tsx
+++ src/container/detail/SimilarMoviesSection.tsx
@@ -1,10 +1,13 @@
+'use client';
 
-import MovieContent from '@/components/movie/MovieContent';
 import SectionTitle from '@/components/SectionTitle';
 import React from 'react';
 import { FaPhotoVideo } from "react-icons/fa";
 import Image from 'next/image';
 import getImagePath from './../../utils/getImagePath';
+import { MOVIE_GENRE_NAME } from '@/constants/movieConstants';
+import { useRouter } from 'next/navigation';
+
 
 interface SimilarMoviesSectionProps {
   movies: any;
@@ -12,7 +15,7 @@ interface SimilarMoviesSectionProps {
 
 const SimilarMoviesSection: React.FC<SimilarMoviesSectionProps> = ({ movies }) => {
 
-  console.log(movies);
+  const { push } = useRouter();
 
   return (
     <section className="px-[5%] max-w-[1600px] mx-auto">
@@ -25,17 +28,23 @@ const SimilarMoviesSection: React.FC<SimilarMoviesSectionProps> = ({ movies }) =
       <ul className='flex flex-wrap gap-5 py-6 px-3'>
         {movies.slice(0, 10).map((movie: any) => {
           return (
-            <li key={movie.id} className='w-[calc((100%-1.25rem*4)/5)] relative'>
-              <div className='w-full pb-[150%]'>
+            <li key={movie.id}
+              onClick={() => push(`/detail/${movie.id}`)}
+              className='w-full sm:w-[calc((100%-1.25rem*1)/2)] md:w-[calc((100%-1.25rem*2)/3)] lg:w-[calc((100%-1.25rem*4)/5)] relative cursor-pointer group hover:scale-105 transition-all duration-300'
+            >
+              <div className='w-full pb-[150%] relative '>
                 <Image
                   fill
-                  className="object-cover object-center"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="object-cover object-center "
                   src={getImagePath(movie.poster_path, "w500") || "/poster_default.jpg"}
                   alt="포스터"
                 />
               </div>
-              <div className='bottom-0 bg-[#03B3E4] w-full h-[100px] z-2'>
-
+              <div className='bg-[#03B3E4] w-full h-[85px] flex flex-col text-[#FFF] py-1 px-3 justify-center 2 group-hover:bg-[#8CCDA2] transition-all duration-300'>
+                <h5 className='font-bold truncate text-[18px]'>{movie.title}</h5>
+                <small className='truncate text-[12px]'>{movie.genre_ids.map((id: number) => MOVIE_GENRE_NAME[id]).join('/')}</small>
+                <small className='truncate text-[12px]'>{movie.release_date}</small>
               </div>
             </li>
           )
