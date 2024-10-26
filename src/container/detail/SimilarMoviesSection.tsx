b11992a0249ea212849c33904c146a059c31c6e2
syh <yhoonsong96@gmail.com>
2024-10-27 00:11:58 +0900

    [Feat] 영화 카드 평점 라벨 구현

--- src/container/detail/SimilarMoviesSection.tsx
+++ src/container/detail/SimilarMoviesSection.tsx
@@ -2,19 +2,19 @@
 
 import SectionTitle from '@/components/SectionTitle';
 import React from 'react';
-import { FaPhotoVideo } from "react-icons/fa";
+import { FaPhotoVideo } from 'react-icons/fa';
 import Image from 'next/image';
 import getImagePath from './../../utils/getImagePath';
 import { MOVIE_GENRE_NAME } from '@/constants/movieConstants';
 import { useRouter } from 'next/navigation';
 
-
 interface SimilarMoviesSectionProps {
   movies: any;
 }
 
-const SimilarMoviesSection: React.FC<SimilarMoviesSectionProps> = ({ movies }) => {
-
+const SimilarMoviesSection: React.FC<SimilarMoviesSectionProps> = ({
+  movies,
+}) => {
   const { push } = useRouter();
 
   return (
@@ -24,40 +24,64 @@ const SimilarMoviesSection: React.FC<SimilarMoviesSectionProps> = ({ movies }) =
         icon={<FaPhotoVideo color={'#03b3e4'} size={28} />}
         text="유사한 영화"
       />
-      {movies.length
-        ?
-        <ul className='flex flex-wrap gap-5 py-6 px-3'>
+      {movies.length ? (
+        <ul className="flex flex-wrap gap-5 py-6 px-3">
           {movies.slice(0, 10).map((movie: any) => {
+            let gradeColor = '#FFFFFF';
+            const gradeColors = ['#FF0000', '#FF4500', '#ffA500', '#9ACD32'];
+            let score = movie.vote_average;
+            try {
+              let level = (Math.ceil(score/2.5) || 1) - 1
+              gradeColor = gradeColors[level]
+            } catch {
+              console.error('Error : Unexpected Range : Score must be number between 0~10');
+            }
             return (
-              <li key={movie.id}
+              <li
+                key={movie.id}
                 onClick={() => push(`/detail/${movie.id}`)}
-                className='w-full sm:w-[calc((100%-1.25rem*1)/2)] md:w-[calc((100%-1.25rem*2)/3)] lg:w-[calc((100%-1.25rem*4)/5)] relative cursor-pointer group hover:scale-105 transition-all duration-300'
+                className="w-full sm:w-[calc((100%-1.25rem*1)/2)] md:w-[calc((100%-1.25rem*2)/3)] lg:w-[calc((100%-1.25rem*4)/5)] relative cursor-pointer group hover:scale-105 transition-all duration-300"
               >
-                <div className='w-full pb-[150%] relative '>
+                <div className="w-full pb-[150%] relative ">
                   <Image
                     fill
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     className="object-cover object-center "
-                    src={getImagePath(movie.poster_path, "w500") || "/film1.jpg"}
+                    src={
+                      getImagePath(movie.poster_path, 'w500') || '/film1.jpg'
+                    }
                     alt="포스터"
                   />
                 </div>
-                <div className='bg-[#03B3E4] w-full h-[85px] flex flex-col text-[#FFF] py-1 px-3 justify-center 2 group-hover:bg-[#8CCDA2] transition-all duration-300'>
-                  <h5 className='font-bold truncate text-[18px]'>{movie.title}</h5>
-                  <small className='truncate text-[12px]'>{movie.genre_ids.map((id: number) => MOVIE_GENRE_NAME[id]).join('/')}</small>
-                  <small className='truncate text-[12px]'>{movie.release_date}</small>
+                <div className="bg-[#03B3E4] w-full h-[85px] flex flex-col text-[#FFF] py-1 px-3 justify-center group-hover:bg-[#8CCDA2] transition-all duration-300">
+                  <h5 className="font-bold truncate text-[18px]">
+                    {movie.title}
+                  </h5>
+                  <small className="truncate text-[12px]">
+                    {movie.genre_ids
+                      .map((id: number) => MOVIE_GENRE_NAME[id])
+                      .join('/')}
+                  </small>
+                  <small className="truncate text-[12px]">
+                    {movie.release_date}
+                  </small>
+                </div>
+                <div className='absolute top-0 vote-clip' style={{background : gradeColor}}>
+                  <span className='absolute top-[2%] right-[4%] sm:top-[2%] sm:right-[3%] text-[#EEEEEE] sm:text-[.8rem]'>
+                    {(score === 10 || score === 0) ? score : score.toFixed(1)}
+                  </span>
                 </div>
               </li>
-            )
+            );
           })}
         </ul>
-        :
-        <div className='flex justify-center items-center text-[#CCCCCC] h-[100px]'>
+      ) : (
+        <div className="flex justify-center items-center text-[#CCCCCC] h-[100px]">
           유사한 영화 정보가 없습니다.
         </div>
-      }
-    </section >
-  )
-}
+      )}
+    </section>
+  );
+};
 
-export default SimilarMoviesSection
+export default SimilarMoviesSection;
