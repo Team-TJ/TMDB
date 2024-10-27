9371b6425c89d21beb92b985c4cc77123c6873f7
YHSONG\yhoon <yhoonsong96@gmail.com>
2024-10-27 14:37:22 +0900

    [Feat] 출연진 모달 로딩 스피너 구현

--- src/container/detail/actor/ActorModal.tsx
+++ src/container/detail/actor/ActorModal.tsx
@@ -21,6 +21,7 @@ const ActorModal: React.FC<ActorModalProps> = ({ actorId, closeModal, params }) 
   const [axios] = useAxios();
   const [actor, setActor] = useState<any>({});
   const [filmo, setFilmo] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [moreState, setMoreState] = useState<any>({ profile: false, filmo: false });
   const [profileHeight, setProfileHeight] = useState(0);
   const [filmoHeight, setFilmoHeight] = useState(0);
@@ -33,6 +34,7 @@ const ActorModal: React.FC<ActorModalProps> = ({ actorId, closeModal, params }) 
     axios.get(`https://api.themoviedb.org/3/person/${actorId}?language=ko-KR`)
       .then(res => {
         setActor(res.data)
+        setLoading((actor && filmo) ? false : true)
       })
     axios.get(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?language=ko-KR`)
       .then(res => {
@@ -49,6 +51,8 @@ const ActorModal: React.FC<ActorModalProps> = ({ actorId, closeModal, params }) 
         })
         array = array.slice(0, 20);
         setFilmo([...array])
+        setLoading((actor && filmo) ? false : true)
+
       })
     document.body.style.overflow = 'hidden';
     return () => {
@@ -95,70 +99,76 @@ const ActorModal: React.FC<ActorModalProps> = ({ actorId, closeModal, params }) 
         </button>
       </div>
 
-      <div className="flex flex-col items-center w-[min(90%,700px)] mx-auto">
-        <div className="w-[220px] h-[350px] md:w-[310px] md:h-[460px] border-white border-solid border-[5px] relative">
-          <Image
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            alt="배우 사진"
-            src={getImagePath(actor.profile_path) || '/no-image.jpg'}
-            className="object-cover object-center"
-          />
+      {loading
+        ?
+        <div className='absolute top-0 loading-container bg-transparent pointer-events-none'>
+          <div className='loading-spinner animate-loading-spin loading-spinner-color-br'></div>
         </div>
+        :
+        <div className="flex flex-col items-center w-[min(90%,700px)] mx-auto">
+          <div className="w-[220px] h-[350px] md:w-[310px] md:h-[460px] border-white border-solid border-[5px] relative">
+            <Image
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt="배우 사진"
+              src={getImagePath(actor.profile_path) || '/no-image.jpg'}
+              className="object-cover object-center"
+            />
+          </div>
 
-        <section className="w-full mt-5">
-          <h3 className="text-[#86C49B] flex items-center font-bold gap-2 pb-2 border-b-[1px] border-[#86C49B]">
-            <IoPersonSharp size={16} />
-            <span className="text-[16px]">PROFILE</span>
-          </h3>
+          <section className="w-full mt-5">
+            <h3 className="text-[#86C49B] flex items-center font-bold gap-2 pb-2 border-b-[1px] border-[#86C49B]">
+              <IoPersonSharp size={16} />
+              <span className="text-[16px]">PROFILE</span>
+            </h3>
 
-          <ul
-            className={cn("bg-[rgba(255,255,255,0.1)] px-5 mt-5 italic overflow-hidden transition-[all_0.5s]")}
-            ref={profileRef}
-            style={{ height: profileHeight }}
-          >
-            <InfoListItem title='이름' content={actor.name} />
-            <InfoListItem title='직업' content={actor.known_for_department} />
-            <InfoListItem title='생일' content={actor.birthday ? `${actor.birthday} ~ ${actor.deathday || 'now'}` : null} />
-            <InfoListItem title='출생지' content={actor.place_of_birth} />
-            {actor.biography ? <InfoListItem content={actor.biography} /> : null}
-          </ul>
+            <ul
+              className={cn("bg-[rgba(255,255,255,0.1)] px-5 mt-5 italic overflow-hidden transition-all duration-500")}
+              ref={profileRef}
+              style={{ height: profileHeight }}
+            >
+              <InfoListItem title='이름' content={actor.name} />
+              <InfoListItem title='직업' content={actor.known_for_department} />
+              <InfoListItem title='생일' content={actor.birthday ? `${actor.birthday} ~ ${actor.deathday || 'now'}` : null} />
+              <InfoListItem title='출생지' content={actor.place_of_birth} />
+              {actor.biography ? <InfoListItem content={actor.biography} /> : null}
+            </ul>
 
-          {actor.biography ?
-            <button className='w-full p-3 text-white hover:underline' onClick={toggleMoreState('profile')}>
-              {moreState.profile ? 'Close -' : 'More +'}
-            </button> : null}
-        </section>
-        s
-        <section className="w-full mt-5">
-          <h3 className="text-[#86C49B] flex items-center font-bold gap-2 pb-2 border-b-[1px] border-[#86C49B]">
-            <IoPersonSharp size={16} />
-            <span className="text-[16px]">FILMOGRAPHY</span>
-          </h3>
+            {actor.biography ?
+              <button className='w-full p-3 text-white hover:underline' onClick={toggleMoreState('profile')}>
+                {moreState.profile ? 'Close -' : 'More +'}
+              </button> : null}
+          </section>
 
-          <ul
-            className="bg-[rgba(255,255,255,0.1)] mt-5 italic overflow-hidden transition-[all_0.5s]"
-            ref={filmoRef}
-            style={{ height: filmoHeight }}
-          >
-            {filmo.map((film: any) => {
-              return (
-                <InfoListItem
-                  key={film.title}
-                  onClick={() => pathName.slice(8) == film.id ? 0 : push(`/detail/${film.id}`)}
-                  className={cn(pathName.slice(8) == film.id ? 'font-bold bg-[#b8b4b42c]' : 'cursor-pointer hover:bg-[#3333338e]')}
-                  title={film.release_date}
-                  content={film.title}
-                />
-              )
-            })}
-          </ul>
-          {filmo.length > 5 ?
-            <button className='w-full p-3 text-white hover:underline' onClick={toggleMoreState('filmo')}>
-              {moreState.filmo ? 'Close -' : 'More +'}
-            </button> : null}
-        </section>
-      </div>
+          <section className="w-full mt-5">
+            <h3 className="text-[#86C49B] flex items-center font-bold gap-2 pb-2 border-b-[1px] border-[#86C49B]">
+              <IoPersonSharp size={16} />
+              <span className="text-[16px]">FILMOGRAPHY</span>
+            </h3>
+
+            <ul
+              className="bg-[rgba(255,255,255,0.1)] mt-5 italic overflow-hidden transition-all duration-500"
+              ref={filmoRef}
+              style={{ height: filmoHeight }}
+            >
+              {filmo.map((film: any) => {
+                return (
+                  <InfoListItem
+                    key={film.title}
+                    onClick={() => pathName.slice(8) == film.id ? 0 : push(`/detail/${film.id}`)}
+                    className={cn(pathName.slice(8) == film.id ? 'font-bold bg-[#b8b4b42c]' : 'cursor-pointer hover:bg-[#3333338e]')}
+                    title={film.release_date}
+                    content={film.title}
+                  />
+                )
+              })}
+            </ul>
+            {filmo.length > 5 ?
+              <button className='w-full p-3 text-white hover:underline' onClick={toggleMoreState('filmo')}>
+                {moreState.filmo ? 'Close -' : 'More +'}
+              </button> : null}
+          </section>
+        </div>}
     </div>
   );
 };
